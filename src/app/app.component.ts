e78a697ae2d2286f901e5188abc4ad24c47467fb
Becker Jean-Christophe <mothos@hotmail.fr>
2024-07-27 14:36:45 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cocktails';
+  counter: number = 0;
+  private subscription!: Subscription;
+
+  ngOnInit() {
+    this.subscription = interval(1000).subscribe(() => {
+      this.counter++;
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
